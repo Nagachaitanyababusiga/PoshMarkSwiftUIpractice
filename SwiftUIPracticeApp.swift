5b684ac0961da5a70932601f64f63f0a2566d848
nagachaitanyababusiga <nagachaitanyababusiga@gmail.com>
2026-01-05 05:34:04 +0530

    adding files

---
+++ SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Sarfaraz on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
